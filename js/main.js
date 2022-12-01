a7fc66c409bb4dce901cbc1d91e4149b1acf923c
Laura Bährs <laura.baehrs@googlemail.com>
2022-12-01 17:46:48 +0100

    create and move obstacles + collision detection

--- js/main.js
+++ js/main.js
@@ -1,53 +1,118 @@
 class Player {
-    constructor(){
-        this.width = 20;
-        this.height = 5;
+  constructor() {
+    this.width = 20;
+    this.height = 5;
 
-        this.positionX = 50 - (this.width / 2);
-        this.positionY = 0;
+    this.positionX = 50 - this.width / 2;
+    this.positionY = 0;
 
-        this.domElement = null;
-        
-        this.createDomElement();
-    }
- 
-    createDomElement(){
-        // step1: create the element
-        this.domElement = document.createElement('div');
+    this.domElement = null;
 
-        // step2: add content or modify (ex. innerHtml)
-        this.domElement.id = 'player';
-        // viewpoint width
-        this.domElement.style.width = this.width + 'vw';
-        // viewpoint height
-        this.domElement.style.height = this.height + 'vh';
-        this.domElement.style.bottom = this.positionY + 'vh';
-        this.domElement.style.left = this.positionX + 'vw';
+    this.createDomElement();
+  }
 
-        // step3: append to the dom: `parentElm.appendChild()`
-        const boardElement = document.getElementById('board');
-        boardElement.appendChild(this.domElement);
-    }
-    doSomething() {
-    }
+  createDomElement() {
+    // step1: create the element
+    this.domElement = document.createElement("div");
 
-    moveLeft() {
-        this.positionX = this.positionX - 5;
-        this.domElement.style.left = this.positionX + "vw";
-    }
+    // step2: add content or modify (ex. innerHtml)
+    this.domElement.id = "player";
+    // viewpoint width
+    this.domElement.style.width = this.width + "vw";
+    // viewpoint height
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.left = this.positionX + "vw";
 
-    moveRight() {
-        this.positionX = this.positionX + 5;
-        this.domElement.style.left = this.positionX + "vw";
-    }
+    // step3: append to the dom: `parentElm.appendChild()`
+    const boardElement = document.getElementById("board");
+    boardElement.appendChild(this.domElement);
+  }
+  doSomething() {}
+
+  moveLeft() {
+    this.positionX--;
+    this.domElement.style.left = this.positionX + "vw";
+  }
+
+  moveRight() {
+    this.positionX++;
+    this.domElement.style.left = this.positionX + "vw";
+  }
+}
+
+class Obstacle {
+  constructor() {
+    this.width = 20;
+    this.height = 10;
+    this.positionX = 50 - this.width / 2;
+    this.positionY = 80;
+
+    this.domElement = null;
+    this.createDomElement();
+  }
+  createDomElement() {
+    // step1: create the element
+    this.domElement = document.createElement("div");
+
+    // step2: add content or modify (ex. innerHtml)
+    this.domElement.className = "obstacle";
+    // viewpoint width
+    this.domElement.style.width = this.width + "vw";
+    // viewpoint height
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+
+    // step3: append to the dom: `parentElm.appendChild()`
+    const boardElement = document.getElementById("board");
+    boardElement.appendChild(this.domElement);
+  }
+  moveDown() {
+    this.positionY--;
+    this.domElement.style.bottom = this.positionY + "vh";
+  }
 }
 
 const player = new Player();
+const obstacles = [];
 
-document.addEventListener('keydown', (event) => {
-    if(event.key === 'ArrowRight') {
-        player.moveRight();
-    } else if (event.key === 'ArrowLeft') {
-        player.moveLeft();
-    }
+// attack event listeners
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    player.moveRight();
+  } else if (event.key === "ArrowLeft") {
+    player.moveLeft();
+  }
 });
+// create obstacles
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  obstacles.push(newObstacle);
+}, 3000);
+
+// move obstacle every 200ms & detect collision
+setInterval(() => {
+  obstacles.forEach((obstacleInstance) => {
+    // move current obstacles
+    obstacleInstance.moveDown();
+
+    // detect if there's a collision between player
+    // and current obstacle
+    // player vs. obstacleInstance
+    // collision detection:
+    if (
+        player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+        player.positionX + player.width > obstacleInstance.positionX &&
+        player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+        player.height + player.positionY > obstacleInstance.positionY
+    ) {
+        console.log("collision detected!!");
+    }
+  });
+
+  //collision detection
+}, 50);
+
+// remove obstacles from array and from dom
+// when they reach bottom
--- planning.md
+++ planning.md
@@ -23,3 +23,49 @@ Milestone 3: make the game more interesting & fix bugs
     - random position for obstacles
     - prevent player from going outside
     - gameover + restart/start button
+
+    
+
+*** Milestone 1: user can move the player left/right *** 
+
+[x] initial setup (html, css, js)
+[x] create git repo
+[x] Player class
+  - positionX
+  - positionY
+  - size
+  - moveLeft()
+  - moveRight()
+[x] add keyboard events
+
+
+
+*** Milestone 2: obstacles appearing in the UI + obstacles move + we detect if there's a collision *** 
+  (note: for this milestone, it's ok if obstacles appear always at the same position) === "shitty game"
+
+
+[ ] Obstacle class
+  - width
+  - height
+  - positionX
+  - positionY
+  - domElement
+  - createDomElement();
+  - moveDown();
+
+[ ] interval to create new obstacles
+[ ] css
+
+
+
+*** Milestone 3: make the game more interesting & fix bugs *** 
+  - random position for obstacles
+  - prevent player from going outside
+
+  - Wishlist:
+    - gameover 
+    - start button 
+    - restart button
+    - multiple lives
+    - score
+    - shooting
